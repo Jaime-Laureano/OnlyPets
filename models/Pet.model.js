5310d80d7e2a73c0420aa37e90251747123aa7f8
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2022-03-18 14:35:50 +0000

    added pet schema

---
+++ models/Pet.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const petSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        species:{ type: String, enum: ['cat', 'dog'], required: true },
+        age: { type: Number, min: 0, max: 30, required: true },
+        size: { type: Number, required: true},
+        weight: { type: Number, min: 0, max: 30, reuqired: true },
+        sex: { type: String, enum: ['male', 'female'] },
+        vaccinated: Boolean,
+    }
+)
+
+const Pet = mongoose.model("Pet", petSchema);
+
+module.exports = Pet;
